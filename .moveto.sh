ca3b077ab537f2cf0f0749c27cad2cdfd044e3ad
Yeharold <1174484433@qq.com>
2020-11-11 22:53:17 +0800

    Update .moveto.sh

--- .moveto.sh
+++ .moveto.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-if [ ! -d "~/homework/$1" ];then mkdir ~/homework/$1;fi;
+if [ ! -d ~/homework/$1 ];then 
+  mkdir ~/homework/$1
+else
+  echo "failed,$1 exists"
+fi
 mv ~/homework/static ~/homework/$1/$(date +"%Y%m%d")
 mkdir ~/homework/static
 echo "seccussfully move to $1"
